a556a27fc3902912a692e8e8755a8d1eeaeae4dd
MarijnVerschuren <marijnverschuren3@gmail.com>
2023-04-03 22:48:16 +0200

    Added controller support

--- controller/main.py
+++ controller/main.py
@@ -1,15 +1,27 @@
 from inputs import *
 import sys, os
+import time
+
+from lib import *  # PS3
 
 
+# entry
 if __name__ == '__main__':
+    # help command
     if "-help" in sys.argv: print(
             "| command              | args      | description                       |",
             "|----------------------|-----------|-----------------------------------|",
-            "| [-controller-list]   |           | list all available controllers    |",
-            "| [-controller]        | %s        | name of the controller            |",
             sep="\n"
         ); exit(0)
-    if "-controller-list" in sys.argv: print(*devices, sep="\n"); exit(0)
+
+    for dev in devices:
+        if dev.name == "Sony PLAYSTATION(R)3 Controller":
+            ps3 = PS3(dev); break
+    else: raise Exception("device not found")
+
+    while 1:
+        ps3.update()
+        if ps3.x_button: print(ps3); sys.stdout.flush()
+
 
     # https://pypi.org/project/aioserial/#quick-start
