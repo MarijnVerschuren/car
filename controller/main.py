ad1c4bec30a15b151565ff034971d209a2833dd8
MarijnVerschuren <marijnverschuren3@gmail.com>
2023-04-03 19:42:38 +0200

    Added arguments

--- controller/main.py
+++ controller/main.py
@@ -1,5 +1,15 @@
-
+from inputs import *
+import sys, os
 
 
 if __name__ == '__main__':
-    print('PyCharm')
+    if "-help" in sys.argv: print(
+            "| command              | args      | description                       |",
+            "|----------------------|-----------|-----------------------------------|",
+            "| [-controller-list]   |           | list all available controllers    |",
+            "| [-controller]        | %s        | name of the controller            |",
+            sep="\n"
+        ); exit(0)
+    if "-controller-list" in sys.argv: print(*devices, sep="\n"); exit(0)
+
+    # https://pypi.org/project/aioserial/#quick-start
